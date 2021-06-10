ce005f08d630ae051716335a7e239f3c3b803ada
kscheltema <k87scheltema@outlook.com>
2021-06-10 07:40:14 +0200

    dynamic logout & login props defined with context

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "../UI/Card/Card";
+import Button from "../UI/Button";
 import classes from "./Home.module.css";
 
 const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
+    ///this onLogout props allows the Button component to be defined in this instance for logout
+    //alternatively this definition would be permanent
   );
 };
 
