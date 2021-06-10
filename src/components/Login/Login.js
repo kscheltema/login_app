762fbfc965b86c40cd4e75591ed4fd105ba0f783
kscheltema <k87scheltema@outlook.com>
2021-06-10 19:59:08 +0200

    rare use example for useImperativeHandle

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useReducer, useEffect, useContext } from "react";
+import React, {
+  useState,
+  useRef,
+  useReducer,
+  useEffect,
+  useContext,
+} from "react";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
@@ -41,6 +47,9 @@ const Login = (props) => {
 
   const authCtx = useContext(AuthContext);
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
   //bad to have and use*** run on an property,
@@ -78,13 +87,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogedIn(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogedIn(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           isValid={emailIsValid}
           id="email"
           htmlFor="email"
@@ -95,6 +111,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           isValid={passwordIsValid}
           id="password"
           htmlFor="password"
@@ -105,7 +122,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
